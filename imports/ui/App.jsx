3a26bfc8caf4313699d001fc17f592e88dd8a1e7
ajaguasin <ajaguasin@gmail.com>
2018-11-02 00:18:53 -0700

    UI for Map and Menu components

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,14 +1,20 @@
 import React, { Fragment } from "react";
 import AccountsUIWrapper from "./components/AccountsWrapper";
+import Map from "./components/main/Map";
+import SimpleMenu from "./components/main/SimpleMenu";
+
 const App = () => (
   <div className="app-wrapper">
     {/* <div className="login-wrapper">
       <AccountsUIWrapper />
     </div> */}
+
+    <SimpleMenu />
     <div>
       <h1>Welcome to Meteor!</h1>
       <p>PHINCHIK</p>
     </div>
+    <Map />
   </div>
 );
 
