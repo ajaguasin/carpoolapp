a3d2fd6bde771c7a9440559dbb6b5fffd01d4f4d
elim322 <einer.lim322@gmail.com>
2018-11-02 11:53:56 -0700

    add material UI styling for select buttons and page

--- imports/ui/components/selection/Selection.js
+++ imports/ui/components/selection/Selection.js
@@ -1,18 +1,34 @@
 import React from "react";
 import Grid from "@material-ui/core/Grid";
+import { withStyles } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
+import styles from "./styles";
 
-const SelectButton = () => {
-  return (
-    <Grid className="buttons">
-      <h2>Are you a:</h2>
-      <div>
-        <button className="driver">Driver</button>
-      </div>
-      <div>
-        <button className="passenger">Passenger</button>
-      </div>
-    </Grid>
-  );
-};
+class SelectButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    const { classes } = this.props;
+    return (
+      <Grid className={classes.buttons}>
+        <h2>Are you a:</h2>
+        <div>
+          <Button variant="contained" className={classes.driver}>
+            Driver
+          </Button>
+          <p>You drive a passenger going in the same direction</p>
+        </div>
+        <div>
+          <Button variant="contained" className={classes.passenger}>
+            Passenger
+          </Button>
+          <p>You carpool with a driver going in the same direction</p>
+        </div>
+      </Grid>
+    );
+  }
+}
 
-export default SelectButton;
+export default withStyles(styles)(SelectButton);
---
+++ imports/ui/components/selection/styles.js
@@ -0,0 +1,20 @@
+const styles = theme => ({
+  buttons: {
+    textAlign: "center",
+    height: "30vh",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "space-between",
+    marginTop: 100
+  },
+  driver: {
+    background: "#58B9D4",
+    width: 300
+  },
+  passenger: {
+    background: "#EC5F59",
+    width: 300
+  }
+});
+
+export default styles;
