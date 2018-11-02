c25e7b192178e6a5d6bae890eb4dc721895f72b3
phinchik <phinchik_27@icloud.com>
2018-11-02 11:06:36 -0700

    working on the login form

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,21 +1,21 @@
-import React, { Fragment } from "react";
+import React, { Fragment, Component } from "react";
 import AccountsUIWrapper from "./components/AccountsWrapper";
 import Map from "./components/main/Map";
 import SimpleMenu from "./components/main/SimpleMenu";
+import HomePage from "./pages/welcome";
 
-const App = () => (
-  <div className="app-wrapper">
-    {/* <div className="login-wrapper">
-      <AccountsUIWrapper />
-    </div> */}
-
-    <SimpleMenu />
-    <div>
-      <h1>Welcome to Meteor!</h1>
-      <p>PHINCHIK</p>
-    </div>
-    <Map />
-  </div>
-);
+class App extends Component {
+  render() {
+    return (
+      <div className="app-wrapper">
+        <div>
+          <SimpleMenu />
+          <HomePage />
+        </div>
+        <Map />
+      </div>
+    );
+  }
+}
 
 export default App;
