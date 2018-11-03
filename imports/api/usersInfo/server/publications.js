9cf91e1a31c73e0da319d80845f1ef29707f2154
Vanessa Bertoletti <vanessabertoletti@gmail.com>
2018-11-03 16:15:44 -0700

    work on meteor method for the input field

--- imports/api/usersInfo/server/publications.js
+++ imports/api/usersInfo/server/publications.js
@@ -5,3 +5,18 @@ if (Meteor.isServer) {
     return UsersInfo.find({});
   });
 }
+
+Meteor.methods({
+  "usersInfo.handleSubmit"() {
+    if (UsersInfo.driver || UsersInfo.passenger === true) {
+      UsersInfo.update(users._id, {
+        $set: {
+          currentLocation: {
+            long: this.current.value,
+            lat: this.current.value
+          }
+        }
+      });
+    }
+  }
+});
