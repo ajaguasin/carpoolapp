2cdbcc5ec1dc44a5af2d0e124460fa3bc1a139a1
Vanessa Bertoletti <vanessabertoletti@gmail.com>
2018-11-05 20:58:39 -0800

    fix header meteor methods

--- imports/api/usersInfo/usersInfo.js
+++ imports/api/usersInfo/usersInfo.js
@@ -10,42 +10,42 @@ export const UsersInfo = new Mongo.Collection("usersInfo");
  * Methods
  *
  */
-Meteor.methods({  
-  
+Meteor.methods({
   "usersInfo.driverToggle"(myUserInfo) {
-  UsersInfo.update(
-    { id: this.userId },
-    {
-      $set: {
-        driver: true
-      }
-    }
-  );
-},
-
-"usersInfo.passengerToggle"() {
-  UsersInfo.update(
-    { id: this.userId },
-    {
-      $set: {
-        passenger: true
-      }
-    }
-  );
-},
-
-"usersInfo.handleSubmit"(coordinates) {
-  console.log(coordinates)
-  UsersInfo.update({id: this.userId}, 
-    { $set: {
-        driverInfo: {
-        currentLocation: {
-          long: coordinates.lng,
-          lat: coordinates.lat
+    UsersInfo.update(
+      { id: this.userId },
+      {
+        $set: {
+          driver: true
         }
-       }
       }
-    })
+    );
+  },
+
+  "usersInfo.passengerToggle"() {
+    UsersInfo.update(
+      { id: this.userId },
+      {
+        $set: {
+          passenger: true
+        }
+      }
+    );
+  },
+
+  "usersInfo.handleSubmit"(coordinates) {
+    console.log(coordinates);
+    UsersInfo.update(
+      { id: this.userId },
+      {
+        $set: {
+          currentLocation: {
+            long: coordinates.lng,
+            lat: coordinates.lat
+          }
+        }
+      }
+    );
   },
   "usersInfo.insertMethod"(userId) {
     UsersInfo.insert({
