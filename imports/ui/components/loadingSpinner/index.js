b0f84f3e2a00da8d6ea50c9c6d4b80f24f4055ab
elim322 <einer.lim322@gmail.com>
2018-11-07 20:51:07 -0800

    style and position loading spinner

--- imports/ui/components/loadingSpinner/index.js
+++ imports/ui/components/loadingSpinner/index.js
@@ -2,17 +2,22 @@ import React from "react";
 import PropTypes from "prop-types";
 import { withStyles } from "@material-ui/core/styles";
 import CircularProgress from "@material-ui/core/CircularProgress";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+
 import styles from "./styles";
 
 function CircularIndeterminate(props) {
   const { classes } = props;
   return (
-    <div>
-      <CircularProgress className={classes.progress} color="secondary" />
-      <p className={classes.loadingText}>
-        Please wait a moment as we look for the next available driver...
-      </p>
-    </div>
+    <Grid justify="center" className={classes.loadingContainer}>
+      <Grid item xs={6} className={classes.progress}>
+        <CircularProgress color="secondary" size={50} />
+        <Typography variant="body1" className={classes.loadingText}>
+          Please wait a moment as we look for the next available driver...
+        </Typography>
+      </Grid>
+    </Grid>
   );
 }
 
