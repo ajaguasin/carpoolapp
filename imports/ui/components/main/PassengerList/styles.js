91b551a1002939e428771e2e0fcf90722a201954
elim322 <einer.lim322@gmail.com>
2018-11-07 21:57:28 -0800

    add buttons for each profile card that appears on passengerlist

--- imports/ui/components/main/PassengerList/styles.js
+++ imports/ui/components/main/PassengerList/styles.js
@@ -1,9 +1,18 @@
 const styles = theme => ({
-  // list: {
-  //   overflow: "scroll",
-  //   width: "90%",
-  //   height: "35%",
-  //   margin: "0 auto"
-  // }
+  list: {
+    overflow: "scroll",
+    width: "90%",
+    height: "35%",
+    margin: "0 auto",
+    display: "flex",
+    flexDirection: "row",
+    flexWrap: "wrap",
+    alignItems: "center"
+  },
+  button: {
+    width: "40%",
+    height: "20%",
+    background: "#29eecb"
+  }
 });
 export default styles;
