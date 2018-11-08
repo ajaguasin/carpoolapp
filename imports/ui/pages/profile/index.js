0abc00593994924eedc3e5cdbba541c54deb66ec
Vanessa Bertoletti <vanessabertoletti@gmail.com>
2018-11-08 13:41:09 -0800

    refactor styles and small details for profile page

--- imports/ui/pages/profile/index.js
+++ imports/ui/pages/profile/index.js
@@ -37,66 +37,79 @@ class Profile extends Component {
             <FontAwesomeIcon className="icon" icon="chevron-left" />
           </Link>
         )}
+        <div className="profileContent">
+          <div className="content">
+            <div className="content__container">
+              <p className="content__container__text">Hello</p>
 
-        <div className="content">
-          <div className="content__container">
-            <p className="content__container__text">Hello</p>
-
-            <ul className="content__container__list">
-              <li className="content__container__list__item">you !</li>
-              {this.props.location.pathname === "/profile" && (
-                <UsersInfoContext.Consumer>
-                  {({ myUserInfo }) => (
-                    <li className="content__container__list__item">
-                      {myUserInfo[0].profileInformation.fullName}
-                    </li>
-                  )}
-                </UsersInfoContext.Consumer>
-              )}
-              {this.props.location.pathname === "/createprofile" && (
-                <li className="content__container__list__item">user !</li>
-              )}
-              <li className="content__container__list__item">beautiful !</li>
-              <li className="content__container__list__item">to carpooling!</li>
-            </ul>
-          </div>
-        </div>
-
-        <form onSubmit={e => this.profileUpdate(e)} className="profileForm">
-          <img
-            alt="company's logo"
-            src="/images/Logo @3x.png"
-            className="logo"
-          />
-          <label className="profileFormLabel">Your Name</label>
-          <input type="text" name="name" className="profileFormInput" />
-
-          <label className="profileFormLabel"> Your Phone Number </label>
-          <input type="text" name="number" className="profileFormInput" />
-
-          <label className="profileFormLabel"> Your Car Model </label>
-          <p className="profileparag">
-            Please fill out this field if you wish to give rides.
-          </p>
-          <input type="text" name="car" className="profileFormInput" />
-          <button type="submit" className="button">
-            SUBMIT
-          </button>
-        </form>
-
-        <UsersInfoContext.Consumer>
-          {({ myUserInfo }) => (
-            <div className="myprofileinfo">
-              {/* <Gravatar email={myUserInfo[0].emails.address} /> */}
-
-              <h1>Name: {myUserInfo[0].profileInformation.fullName}</h1>
-              <p>
-                Phone Number: {myUserInfo[0].profileInformation.phoneNumber}
-              </p>
-              <p>Car Model: {myUserInfo[0].profileInformation.carModel}</p>
+              <ul className="content__container__list">
+                <li className="content__container__list__item">you !</li>
+                {this.props.location.pathname === "/profile" && (
+                  <UsersInfoContext.Consumer>
+                    {({ myUserInfo }) => (
+                      <li className="content__container__list__item">
+                        {myUserInfo[0].profileInformation.fullName} !
+                      </li>
+                    )}
+                  </UsersInfoContext.Consumer>
+                )}
+                {this.props.location.pathname === "/createprofile" && (
+                  <li className="content__container__list__item">new user !</li>
+                )}
+                <li className="content__container__list__item">beautiful !</li>
+                <li className="content__container__list__item">
+                  to carpooling!
+                </li>
+              </ul>
             </div>
-          )}
-        </UsersInfoContext.Consumer>
+          </div>
+
+          <form onSubmit={e => this.profileUpdate(e)} className="profileForm">
+            <img
+              alt="company's logo"
+              src="/images/Logo @3x.png"
+              className="logo"
+            />
+            <label className="profileFormLabel">Your Name</label>
+            <input type="text" name="name" className="profileFormInput" />
+
+            <label className="profileFormLabel"> Your Phone Number </label>
+            <input type="text" name="number" className="profileFormInput" />
+
+            <label className="profileFormLabel"> Your Car Model </label>
+            <p className="profileparag">
+              Please fill out this field if you wish to give rides.
+            </p>
+            <input type="text" name="car" className="profileFormInput" />
+            <button type="submit" className="button">
+              SUBMIT
+            </button>
+          </form>
+
+          <UsersInfoContext.Consumer>
+            {({ myUserInfo }) =>
+              myUserInfo[0].email && (
+                <div className="myprofileinfo">
+                  <Gravatar
+                    email={myUserInfo[0].email.address}
+                    default="robohash"
+                    className="avatar"
+                  />
+                  <div className="infoparag">
+                    <p>Name: {myUserInfo[0].profileInformation.fullName}</p>
+                    <p>
+                      Phone Number:{" "}
+                      {myUserInfo[0].profileInformation.phoneNumber}
+                    </p>
+                    <p>
+                      Car Model: {myUserInfo[0].profileInformation.carModel}
+                    </p>
+                  </div>
+                </div>
+              )
+            }
+          </UsersInfoContext.Consumer>
+        </div>
       </div>
     );
   }
