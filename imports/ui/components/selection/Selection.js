44cf4cfbf998a9814cb127bd7d4c0e0257f02736
elim322 <einer.lim322@gmail.com>
2018-11-08 16:14:23 -0800

    add animation to select page

--- imports/ui/components/selection/Selection.js
+++ imports/ui/components/selection/Selection.js
@@ -6,6 +6,8 @@ import styles from "./styles";
 import { Link } from "react-router-dom";
 import Tooltip from "@material-ui/core/Tooltip";
 import Typography from "@material-ui/core/Typography";
+import Fade from "@material-ui/core/Fade";
+import Slide from "@material-ui/core/Slide";
 
 class SelectButton extends React.Component {
   constructor(props) {
@@ -27,32 +29,38 @@ class SelectButton extends React.Component {
     myUserInfo && console.log(myUserInfo);
     return (
       <Grid className={classes.buttons}>
-        <Typography className={classes.title}>
-          Who are <br /> you?
-        </Typography>
+        <Fade in={true} timeout={2000}>
+          <Typography className={classes.title}>
+            Who are <br /> you?
+          </Typography>
+        </Fade>
         <div className={classes.selectButtons}>
-          <Tooltip title="You drive a passenger going in the same direction">
-            <Link to="/main">
-              <button
-                onClick={() => this.driverToggle(myUserInfo)}
-                variant="contained"
-                className={classes.driver}
-              >
-                Driver
-              </button>
-            </Link>
-          </Tooltip>
-          <Tooltip title="You carpool with a driver going in the same direction">
-            <Link to="/main">
-              <button
-                onClick={() => this.passengerToggle(myUserInfo)}
-                variant="contained"
-                className={classes.passenger}
-              >
-                Passenger
-              </button>
-            </Link>
-          </Tooltip>
+          <Slide in={true} direction="up" timeout={1000}>
+            <Tooltip title="You drive a passenger going in the same direction">
+              <Link to="/main">
+                <button
+                  onClick={() => this.driverToggle(myUserInfo)}
+                  variant="contained"
+                  className={classes.driver}
+                >
+                  Driver
+                </button>
+              </Link>
+            </Tooltip>
+          </Slide>
+          <Slide in={true} direction="up" timeout={1000}>
+            <Tooltip title="You carpool with a driver going in the same direction">
+              <Link to="/main">
+                <button
+                  onClick={() => this.passengerToggle(myUserInfo)}
+                  variant="contained"
+                  className={classes.passenger}
+                >
+                  Passenger
+                </button>
+              </Link>
+            </Tooltip>
+          </Slide>
         </div>
       </Grid>
     );
